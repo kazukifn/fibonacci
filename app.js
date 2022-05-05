e6006e48e92e1205a0da66757e12a4e0525646a7
kazukifn <oknsdnhrkm56@gmail.com>
2022-05-05 12:38:51 +0900

    トリボナッチ数列を出力する関数を実装

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 'use strict';
+
 const memo = new Map();
+const length = 40;
 memo.set(0, 0);
-memo.set(1, 1);
+memo.set(1, 0);
+memo.set(2, 1);
+
+/**
+ * トリボナッチ数列を出力する
+ * @param {int} n 
+ * @returns {int} value
+ */
 function fib(n) {
     if (memo.has(n)) {
         return memo.get(n);
     }
-    const value = fib(n - 1) + fib(n - 2);
+    const value = fib(n - 1) + fib(n - 2) + fib(n - 3);
     memo.set(n, value);
     return value;
 }
-const length = 40;
+
 for (let i = 0; i <= length; i++) {
     console.log(fib(i));
 }
